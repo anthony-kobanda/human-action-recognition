c0fad6b6d41286896b22c85aa44a5af390d1eb58
AnthonyKobanda <anthony.kobanda@gmail.com>
2022-01-18 15:31:13 +0100

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # **Human Action Recognition in Videos using Pedestrian Keypoints**
-*Case study in Artificial Intelligence and Visual Computing as part of a [group school project](https://moodle.polytechnique.fr/course/view.php?id=13078)*.
+*Case study in Artificial Intelligence and Visual Computing as part of a [group school project](https://moodle.polytechnique.fr/course/view.php?id=13078) with [IDEMIA](https://www.idemia.com/)*.
 
 ___
 ___
@@ -15,12 +15,11 @@ ___
 - [**Conclusion**](#conclusion)
 - [**References**](#references)
 
-
 ___
 
 ## **Introduction**
 
-Classifying pedestrians' every-day life actions is a state-of-the art topic in computer vision, including a lot of sub-topics like pedestrian detection, segmentation, video consolidation, classification, etc.
+Classifying pedestrians' every-day life actions is an interesting topic in computer vision, including a lot of sub-topics like pedestrian detection and segmentation, video consolidation, classification, etc.
 
 To recognize an action, a human being would localize the person first, analyze the position of its body-parts, see how these are moving and interacting,    and classify accordingly.
 
@@ -34,7 +33,8 @@ ___
 ### ***Presentation***
 
 The dataset used is the **NTU RGB+D - Action Recognition Dataset (Skeletons)** described in [this repository](https://github.com/shahroudy/NTURGB-D).
-When cloning the current respository please download the following [zip file](https://drive.google.com/u/0/uc?export=download&confirm=7nHU&id=1CUZnBtYwifVXS21yVg62T-vrPVayso5H) (5.8 Go) and extract it (13.4 Go) in the data folder. This compressed folder contains the relevant data for our project.
+
+When cloning the current respository please also download the following [zip file](https://drive.google.com/u/0/uc?export=download&confirm=7nHU&id=1CUZnBtYwifVXS21yVg62T-vrPVayso5H) (5.8 Go zipped and 13,4 Go unzipped) and extract it in the data folder. This compressed folder contains the relevant data for our project. Once it is done, run ```data_cleaning.py``` to format the data correctly.
 
 ### ***Structure***
 
@@ -68,26 +68,32 @@ The dataset contains 60 action classes listed below:
 | A25. reach into pocket                           | A26. hopping (one foot jumping)       | A27. jump up                              |
 | A28. make a phone call/answer phone              | A29. playing with phone/tablet        | A30. typing on a keyboard                 |
 | A31. pointing to something with finger           | A32. taking a selfie                  | A33. check time (from watch)              |
-| A34. rub two hands together                      | A35. nod head/bow                     | A36. shake head                           | 
+| A34. rub two hands together                      | A35. nod head/bow                     | A36. shake head                           |
 | A37. wipe face                                   | A38. salute                           | A39. put the palms together               |
 | A40. cross hands in front (say stop)             | A41. sneeze/cough                     | A42. staggering                           |
 | A43. falling                                     | A44. touch head (headache)            | A45. touch chest (stomachache/heart pain) |
-| A46. touch back (backache)                       | A47. touch neck (neckache)            | A48. nausea or vomiting condition         | 
+| A46. touch back (backache)                       | A47. touch neck (neckache)            | A48. nausea or vomiting condition         |
 | A49. use a fan (with hand or paper)/feeling warm | A50. punching/slapping other person   | A51. kicking other person                 |
 | A52. pushing other person                        | A53. pat on back of other person      | A54. point finger at the other person     |
 | A55. hugging other person                        | A56. giving something to other person | A57. touch other person's pocket          |
-| A58. handshaking                                 | A59. walking towards each other       |  A60. walking apart from each other       |
+| A58. handshaking                                 | A59. walking towards each other       | A60. walking apart from each other        |
 |                                                  |                                       |                                           |
 
 ___
 
 ## **Analysis and Visualization of the Dataset**
 
-*TODO*
+For the purpose of this project we will only consider the following actions: A6, A7, A8, A9, A15, A25, A31, A33, A43.
+
+The analysis of the dataset is done [here](./data_analysis.ipynb).
+
+The visualization of the dataset is done [here](./dta_visualization.ipynb).
 
 ___
 
 ## **Models**
+
+To perform the task aforementioned we decided to implement LSTM (*Long Short Term Memory*) networks ([here](./models_implemented/lstm.ipynb)). Indeed, given the sequential aspect of our data it seems relevant to consider models that would take into account such specificity.
 
 ___
 
--- data_cleaning.py
+++ data_cleaning.py
@@ -10,50 +10,76 @@ root_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = root_dir + "/data/"
 
 
+# all the skeletons file names
+complete_set = set(os.listdir(data_dir + "nturgbd_skeletons_s001_to_s017/"))
 
+
+# some files have missing skeletons or lead to issues
 with open(data_dir + "missing_skeletons.txt", 'r') as missing_skeletons_file:
-    
-    # only the file with non-missing skeletons are considered
-    raw_file_names = list(set(os.listdir(data_dir + "nturgb+d_skeletons/")) - set([file_name.replace('\n','') + ".skeleton" for file_name in missing_skeletons_file.readlines()]))
+    missing_set = set([file_name.replace('\n','') + ".skeleton" for file_name in missing_skeletons_file.readlines()])
     missing_skeletons_file.close()
-
-nb_files = len(raw_file_names)
-
-cleaned_file_names = os.listdir(data_dir + "nturgb+d_skeletons_cleaned/")
-
+with open(data_dir + "issue_skeletons.txt", 'r') as issue_skeletons_file:
+    issue_set = set([file_name.replace('\n','') + ".skeleton" for file_name in issue_skeletons_file.readlines()])
+    issue_skeletons_file.close()
 
 
+# we discard the previous files from the complete set of files 
+filtered_set = complete_set - missing_set - issue_set
+nb_files = len(filtered_set)
+
+# it is more convenient to manipulate lists for what follows
+complete_set = list(complete_set)
+missing_set = list(missing_set)
+issue_set = list(issue_set)
+filtered_set = list(filtered_set)
+
+
+# creation of the final folder for the data
+if "nturgbd_skeletons_cleaned" not in os.listdir(data_dir):
+    os.mkdir(data_dir + "nturgbd_skeletons_cleaned/")
+cleaned_set = os.listdir(data_dir + "nturgbd_skeletons_cleaned/")
+
+
+# we now process to the cleaning of the files in the filtered set
 for i in tqdm(range(nb_files)):
 
-    if raw_file_names[i].replace(".skeleton", ".npy") not in cleaned_file_names:
+    if filtered_set[i].replace(".skeleton", ".npy") not in cleaned_set:
 
-        with open(data_dir + "nturgb+d_skeletons/" + raw_file_names[i], 'r') as raw_file:
+        with open(data_dir + "nturgbd_skeletons_s001_to_s017/" + filtered_set[i], 'r') as raw_file:
             file_lines = raw_file.readlines()
+            nb_frames = int(file_lines[0][:-1])
             raw_file.close()
-
-        number_of_frames = int(file_lines[0][:-1])
         
-        if number_of_frames == (len(file_lines)-1)/28:
+        if nb_frames == (len(file_lines)-1)/28:
             
             try:
 
                 # we only keep the spatial coordinate for each of the 25 joints at each frame 
-                file_data = np.array([[file_lines[j].split(' ')[:3] for j in range(4+28*i,1+28*(i+1))] for i in range(number_of_frames)], dtype=float)
+                cleaned_file_data = np.array([[file_lines[j].split(' ')[:3] for j in range(4+28*i,1+28*(i+1))] for i in range(nb_frames)], dtype=float)
 
-                # we save the data as a numpy array of shape (number_of_frames, number_of_joints, number_of_spatial_coordinates) = (number_of_frame, 25, 3) 
-                with open(data_dir + "nturgb+d_skeletons_cleaned/" + raw_file_names[i].replace(".skeleton", ".npy"), 'wb') as cleaned_file:
-                    np.save(cleaned_file, file_data)
+                # we save the data as a numpy array of shape: (nb_frames, nb_joints, nb_spatial_coordinates) = (nb_frames, 25, 3) 
+                with open(data_dir + "nturgbd_skeletons_cleaned/" + filtered_set[i].replace(".skeleton", ".npy"), 'wb') as cleaned_file:
+                    np.save(cleaned_file, cleaned_file_data)
                     cleaned_file.close()
             
-            # some files, including missing skeletons, lead to some issues we will solve later (around 12000 over 57000)
+            # the file leads to an issue so we add its name in the corresponding file
             except:
 
-                with open(data_dir + "issue_skeletons.txt", 'a') as issue_skeletons_file:
-
-                    if i==0:
-                        issue_skeletons_file.write(raw_file_names[i].replace(".skeleton", ""))
+                if filtered_set[i].replace(".skeleton", "") not in issue_set:
+                    with open(data_dir + "issue_skeletons.txt", 'a') as issue_skeletons_file:
+                        line_content = filtered_set[i].replace(".skeleton", "")
+                        if i>0:
+                            line_content = '\n' + line_content
+                        issue_skeletons_file.write(line_content)
                         issue_skeletons_file.close()
-                    else:                
-                        issue_skeletons_file.write('\n' + raw_file_names[i].replace(".skeleton", ""))
+        
+        # the file leads to an issue (nb_frames doesn't match the number of lines) so we add its name in the corresponding file
+        else:
 
-                pass
+            if filtered_set[i].replace(".skeleton", "") not in issue_set:
+                with open(data_dir + "issue_skeletons.txt", 'a') as issue_skeletons_file:
+                    line_content = filtered_set[i].replace(".skeleton", "")
+                    if i>0:
+                        line_content = '\n' + line_content
+                    issue_skeletons_file.write(line_content)
+                    issue_skeletons_file.close()
